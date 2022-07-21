8fa8eb774835e85aa8104f0d50365eb16a9d25f9
SiunKimm <nvidiayi@gmail.com>
2022-07-21 18:03:28 +0900

    just test cm

--- Vuex/core/src/App.vue
+++ Vuex/core/src/App.vue
@@ -1,36 +1,35 @@
 <template>
-<!-- App.vue(Parent) -->
-<div id="app">
-  Parent counter : {{ counter }} <br>
-  <button @click="addCounter">+</button>
-  <button @click="subCounter">-</button>
-  <!-- Child 컴포넌트를 등록하고 counter 데이터 속성을 props로 전달한다. -->
-  <child v-bind:num="counter"></child>
-</div>
+    <div>
+        <h1>Count: {{ count }}</h1>
+        <h1>Double: {{ double }}</h1>
+        <button @click="increase">increase</button>
+        <button @click="decrease">decrease</button>
+    </div>
 </template>
 
 <script>
-// App.vue(Parent)
-import ChildV from "./ChildV.vue";
+import { reactive, computed } from '@vue/composition-api';
+
+const useCount = () => {
+    const count = ref(0);
+    const double = computed(() => count.value * 2);
+
+    const increase = () => ++state.count;
+    const decrease = () => --state.count;
+
+    return { count, double, increase, decrease }
+}
 
 export default {
-  components: {
-    // Child 컴포넌트를 하위 컴포넌트로 등록
-    child: ChildV
-  },
-  data() {
-    return {
-      counter: 0
-    };
-  },
-  methods: {
-    // 이벤트 추가
-    addCounter() {
-      this.counter++;
-    },
-    subCounter() {
-      this.counter--;
+    setup () {
+        const { count, double, increase, decrease } = useCount();
+
+        return {
+            count,
+            double,
+            increase,
+            decrease
+        }
     }
-  }
-};
+}
 </script>
